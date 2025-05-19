fe8beed5768c7930879df4f29f71e83e84c8ee50
yama-8080 <120051428+yama-8080@users.noreply.github.com>
2025-05-19 23:19:52 +0900

    modify 16_phdf5_write.cpp

--- 09_io/16_phdf5_write.cpp
+++ 09_io/16_phdf5_write.cpp
@@ -14,14 +14,14 @@ int main (int argc, char** argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
   assert(mpisize == dim[0]*dim[1]);
   hsize_t N[2] = {NX, NY};
+  hsize_t Nlocal[2] = {NX/dim[0], NY/dim[1]};
 
-  hsize_t Nlocal[2] = {NX/(dim[0]*2), NY/(dim[1]*2)};
+  hsize_t block[2] = {Nlocal[0]/2, Nlocal[1]/2};
   hsize_t offset[2] = {mpirank / dim[0], mpirank % dim[0]};
-  for(int i=0; i<2; i++) offset[i] *= Nlocal[i];
+  for(int i=0; i<2; i++) offset[i] *= block[i];
   hsize_t count[2] = {2,2};
-  hsize_t stride[2] = {NX/dim[0], NY/dim[1]};
-  //printf("rank=%d: offset=(%d,%d), stride=(%d,%d)\n", mpirank, offset[0], offset[1], stride[0], stride[1]);
-  
+  hsize_t stride[2] = {Nlocal[0], Nlocal[1]};
+
   vector<int> buffer(Nlocal[0]*Nlocal[1],mpirank);
   hid_t plist = H5Pcreate(H5P_FILE_ACCESS);
   H5Pset_fapl_mpio(plist, MPI_COMM_WORLD, MPI_INFO_NULL);
@@ -29,8 +29,9 @@ int main (int argc, char** argv) {
   hid_t globalspace = H5Screate_simple(2, N, NULL);
   hid_t localspace = H5Screate_simple(2, Nlocal, NULL);
   hid_t dataset = H5Dcreate(file, "dataset", H5T_NATIVE_INT, globalspace,
-			    H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-  H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, Nlocal);
+                            H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+  //H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, Nlocal);
+  H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, block);
   H5Pclose(plist);
   plist = H5Pcreate(H5P_DATASET_XFER);
   H5Pset_dxpl_mpio(plist, H5FD_MPIO_COLLECTIVE);
